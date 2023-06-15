eebf743a7282a3b1fbe61f6e99d00b064cd89783
Felipe <felipemmel@gmail.com>
2023-06-15 14:54:45 -0300

    :tada: intial commit

---
+++ .vscode/script.js
@@ -0,0 +1,31 @@
+function toggleMode() {
+const html = document.documentElement
+
+html.classList.toggle("light")
+
+// pegar a tag img
+const img = document.querySelector("#profile img")
+
+// substituir a imagem
+if (html.classList.contains("light")) {
+//se tiver light mode , adicionar a imagem light
+img.setAttribute("src", "./assets/diablo.png")
+
+
+ } else {
+//se tiver sem light mode , adicionar a imagem normal
+img.setAttribute("src", "./assets/avatar.png")
+
+ }
+
+
+
+
+
+
+
+
+
+
+}
+
